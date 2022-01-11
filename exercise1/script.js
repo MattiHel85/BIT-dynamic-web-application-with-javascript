791b6ab5c6c09293388ae4c0b78bf217087fa1b3
Matt Simpson <matt.rc.simpson@gmail.com>
2022-01-11 11:53:57 +0200

    add answers for first lesson to js file

--- exercise1/script.js
+++ exercise1/script.js
@@ -1,1 +1,108 @@
 console.log("Welcome to the course!");
+
+// function myFunction() {
+//     alert("Sivu ladattu onnistuneesti!");
+// }
+
+// Answers
+// 1. JS Output and console
+    // alert("Hello World!");
+    
+// 2. Defining variables
+//     var/let/const
+
+    // let firstName = "Matt";
+    // let lastName = "Simpson";
+
+    // document.write("<p>" + firstName + " " + lastName + "</p>" )
+    // console.log(firstName + " " + lastName);
+
+// 3. Conditionals
+    // let x = 17;
+    // let order = "Beer";
+
+    // if (x > 50) {
+    //     document.write("<h1>He's over 50!</h1>");
+    // } else if (x <= 50 && x > 30) {
+    //     document.write("<strong>Middle aged man, who ordered some " + order.toLowerCase() + ".</strong>");
+    // } else if (x <=30 && x > 20) {
+    //     document.write("<strong>You're too young to drink " + order.toLowerCase() + " in my pub!</strong>");
+    // } else if (x <= 20 && x > 17) {
+    //     document.write("<strong>You think you're getting some " + order.toLowerCase() + "? Do one!</strong>");
+    // } else
+    // document.write("<strong>GET OUT YOU WEE BAIRN!!! YOU CAN GET SOME " + order.toUpperCase() + " FROM YOUR MAMMYS ALCOHOL CABINET!!!</strong>");
+
+// 4. Loops
+
+    // This is not correct.
+
+    // var arr = [];
+
+    // for (var i=1; i<100; i++) {
+    //     arr.push(Math.floor(Math.random() * i ) );
+    //     document.write("<p>" + arr[i -1]+ "</p>");
+    // }
+
+// 5. Functions walkthrough
+    // function areYouOldEnough(age) {
+    //     if(age < 18) {
+    //         console.log("Sorry, you're not old enough yet");
+    //         return false;
+    //     } else
+    //     console.log(true);
+    //     alert("You're of age!");
+    //     return true;
+    // }
+
+
+// 6. Objects
+
+// var personArray = [
+//     personOne = {
+//         firstName: "Dave",
+//         lastName: "England"
+//     },
+//     personTwo = {
+//         firstName: "Stephen",
+//         lastName: "Wales"
+//     },
+//     personThree = {
+//         firstName: "Terry",
+//         lastName: "Scotland"
+//     },
+//     personFour = {
+//         firstName: "Brian",
+//         lastName: "Ireland"
+//     }
+// ]
+
+// for (let i in personArray) {
+//     console.log(personArray[i].firstName + " " + personArray[i].lastName)
+// }
+
+// Independent exercises 1
+
+// 1. Dogs age
+
+// function dogsAge(humanAge) {
+//     ageInDogYears = humanAge * 7;
+//     document.write("<strong>Your age in dog years is " + ageInDogYears + ".</strong>");
+// }
+
+// 2. Debugging a script 
+// var i = 5;
+// var j = 10;
+// var k = 25;
+// var l = "Joe";
+
+// if (i < j) {
+//     console.log("i");
+// } else if (j > k) {
+//     console.log("k");
+// } else console.log("j");
+
+// var arr = [3, 5, 7];
+
+// for(var i=0;i<=arr.length; i++) {
+//     console.log(arr[i]);
+// }
